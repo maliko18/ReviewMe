80a39baff751d2f54f309adb838b8339ef87813a
Your Name <you@example.com>
2024-12-04 14:12:57 +0100

    models and migrations setup

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Restaurant extends Model
+{
+    /** @use HasFactory<\Database\Factories\RestaurantFactory> */
+    use HasFactory;
+
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+
+}
---
+++ database/seeders/AddBannerSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AddBannerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
